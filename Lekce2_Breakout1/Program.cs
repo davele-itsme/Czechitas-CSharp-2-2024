1e2f77a8329fb37232589f9a39c97c500afd1d16
David Le <ledaodavid@gmail.com>
2024-04-18 19:10:14 +0200

    Lekce 2, breakout 1

---
+++ Lekce2_Breakout1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Zbyvajici dny do dovolene
+// Postupne se uzivatele zeptej na den, mesic a rok odjezdu na dovolenou. Pomoci DateTime a TimeSpan vypocitej pocet
+// zbyvajicich dni do odjezdu na dovolenou a vypis je na konzoli.
+// Pozn.: pro zjednoduseni staci pouzit int.Parse
+
+Console.WriteLine("Zadej den");
+string textDen = Console.ReadLine();
+int den = int.Parse(textDen);
+
+Console.WriteLine("Zadej mesic");
+string textMesic = Console.ReadLine();
+int mesic = int.Parse(textMesic);
+
+Console.WriteLine("Zadej rok");
+string textRok = Console.ReadLine();
+int rok = int.Parse(textRok);
+
+DateTime ted = DateTime.Now;
+DateTime casDovoleny = new DateTime(rok, mesic, den);
+
+TimeSpan casDoDovoleny = ted - casDovoleny;
+
+Console.WriteLine($"Dny: {casDoDovoleny.Days}, Minuty: {casDoDovoleny.Minutes}");
+
+Console.WriteLine(ted.ToString());
+Console.WriteLine(ted.ToString("yyyy-MM-dd hh:mm:ss"));
+Console.WriteLine(ted.ToString("dd MM yyyy hh:mm:ss"));
