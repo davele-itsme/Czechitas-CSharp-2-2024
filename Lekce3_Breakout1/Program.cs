350967566ffc1fa9af3cafeaf810fc8105b3276c
David Le <ledaodavid@gmail.com>
2024-04-25 17:14:40 +0200

    Pridani ukolu do breakout roomky

---
+++ Lekce3_Breakout1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+					
+public class Program
+{
+    public static void Main()
+    {
+				
+        //Ukol - Obratte poradi stringu
+        string palindrom = "Kuna nese nanuk";
+		
+        //Ukol - Napiste funkci, ktera umi detekovat, ze se jedna o palindrom (zatim jen u slov) a pak z pole vypiste jen palindromy
+        string[] slova = new string[] {"kajak", "program", "rotor", "Czechitas", "madam", "Jarda", "radar", "nepotopen"};
+		
+        //Ukol - opravte v tomto textu omylem zapnuty Caps Lock
+        string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
+		
+        //Ukol - rozsifrujte tuto zpravu - text byl zasifrovan tak, ze jsme kazde pismeno posunuli o jedno doprava: 'a' -> 'b'. 
+        string sifra = "Wzcpsob!qsbdf!.!hsbuvmvkj!b!ktfn!ob!Ufcf!qztoz";
+		
+		
+    }
+}
