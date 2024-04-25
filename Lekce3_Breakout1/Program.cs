82e03e9d41aec1ca0c7f6804bb738ee576107947
David Le <ledaodavid@gmail.com>
2024-04-25 19:16:29 +0200

    Reseni prvniho prikladu

--- Lekce3_Breakout1/Program.cs
+++ Lekce3_Breakout1/Program.cs
@@ -4,19 +4,48 @@ public class Program
 {
     public static void Main()
     {
-				
+
         //Ukol - Obratte poradi stringu
-        string palindrom = "Kuna nese nanuk";
-		
+        string palindrom = "Kuna nese nanuk"; //15 charakteru
+        string obracenyString = "";
+        for (int i = palindrom.Length; i > 0; i--)
+        {
+            //Console.WriteLine(palindrom[i - 1]);
+            obracenyString += palindrom[i - 1];
+        }
+
+        Console.WriteLine(obracenyString);
+
+        int[] pole = new[] { 4, 5, 6 };
+        int soucet = 0;
+        for (int i = 0; i < pole.Length; i++)
+        {
+            soucet += pole[i];
+            //index zacina nulou
+        }
+
         //Ukol - Napiste funkci, ktera umi detekovat, ze se jedna o palindrom (zatim jen u slov) a pak z pole vypiste jen palindromy
-        string[] slova = new string[] {"kajak", "program", "rotor", "Czechitas", "madam", "Jarda", "radar", "nepotopen"};
-		
+        string[] slova = new string[] { "kajak", "program", "rotor", "Czechitas", "madam", "Jarda", "radar", "nepotopen" };
+
+        static bool JePalindrom(string slovo)
+        {
+            return true;
+        }
+
+
+        for (int i = 0; i < slova.Length; i++)
+        {
+            
+        }
+
+
+
+
         //Ukol - opravte v tomto textu omylem zapnuty Caps Lock
         string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
-		
+
         //Ukol - rozsifrujte tuto zpravu - text byl zasifrovan tak, ze jsme kazde pismeno posunuli o jedno doprava: 'a' -> 'b'. 
         string sifra = "Wzcpsob!qsbdf!.!hsbuvmvkj!b!ktfn!ob!Ufcf!qztoz";
-		
-		
+
     }
 }
