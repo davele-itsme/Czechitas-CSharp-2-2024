2cacc2da187317d651f8c7f65de056534f700a82
David Le <ledaodavid@gmail.com>
2024-05-02 18:54:40 +0200

    BR1

---
+++ Lekce4_Breakout1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static public void Main()
+    {
+        List<double> cisla = new List<double>
+        {
+            0.046939913,
+            0.295866297,
+            0.852489925,
+            0.84994766,
+            0.96925183,
+            0.946275497,
+            0.688903175,
+            0.553463564,
+            0.59628254,
+            0.645816929
+        };
+
+        // Vypis vsechna cisla na konzoli (nachystej si pro to funkci)
+        Vypis(cisla);
+
+        // Vypis na konzoli pocet cisel v seznamu
+        VypisPocet(cisla);
+
+        // Pridej cislo 0.5 do seznamu
+
+        // vypis prvni cislo ze seznamu, ktere je vetsi nez 0.8
+
+        // najdi nejvetsi cislo v seznamu, vypis, ktere to je, a odstran ho ze seznamu
+
+        // vypis opet vsechna cisla a jejich pocet
+    }
+}
