869360d57652a9eb9f1f5f8b2a22faadf0283ee4
David Le <ledaodavid@gmail.com>
2024-05-02 19:41:39 +0200

    BR2 - priprava

---
+++ Lekce4_Breakout2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/* Vytvorte tridu TelefonniSeznam
+- uvnitr ni budete ukladat dvojice Jmeno - Telefonni cislo
+- k tomu pouzijte Dictionary, jehoz klice budou stringy a hodnoty integery
+- v ramci konstruktoru vyplnte nekolik jmen a cisel
+- vytvorte metodu, ktera do slovniku ulozi noveho cloveka a jeho telefonni cislo
+- vytvorte metodu, ktera vrati telefonni cislo cloveka
+- vytvorte metodu, ktera vypise cely telefonni seznam
+- vytvorte metodu, ktera smaze daneho cloveka
+- vytvorte metodu, ktera smaze cely telefonni seznam .Clear()
+- myslete na mozne chybove stavy (co kdyz tam dany clovek neni, co kdyz pridavam cloveka, ktery uz tam je)
+- napiste kratky program, ktery overi funkcnost teto tridy */
+
+public class Program
+{
+    public static void Main()
+    {
+
+    }
+}
