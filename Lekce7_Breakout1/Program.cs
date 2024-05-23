10da0babb8d624af58ca689df1e64bc2c1656111
David Le <ledaodavid@gmail.com>
2024-05-23 17:19:26 +0200

    Lekce7 - nove zadani

---
+++ Lekce7_Breakout1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+					
+public class Program
+{
+    public static void Main()
+    {
+        //navrhnete vhodnou strukturu trid, ktera umozni v programu (v Mainu) vytvorit seznam zviratek v zoo koutku (List), 
+        //ktery pak muzeme jednoduse projit cyklem (foreach) a zadat kazdemu z nich prikaz VydavejZvuk
+        //pritom kazde zviratko bude vydavat jiny zvuk (vypise na konzoli Haf, haf nebo Mnau, mnau)
+        //v ramci cyklu nechci resit, jake konkretni zviratko to je
+        //vytvorte alespon 3 ruzna zviratka
+        //hint: budete potrebovat vhodnou bazovou tridu a virtual/abstract a override
+    }
+}
