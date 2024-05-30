6238e9ad3e2e8867ee649bdc673aa8d0ece67158
David Le <ledaodavid@gmail.com>
2024-05-30 18:49:22 +0200

    Breakout1 Lekce8 zadani

---
+++ Lekce8_Breakout1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+					
+public class Program
+{
+    public static void Main()
+    {
+        // Vytvořte abstraktní třídu PohadkovaBytost a v ní abstraktní metodu NapisJakTravisVolnyCas
+        // Vytvorte tridy Princezna, Rytir, Carodenice, ktere dedi PohadkovouBytost
+        // Naimplementujte metodu NapisJakTravisVolnyCas - vypsanemu textu na konzoli se meze nekladou
+        // Vytvorte instance od kazde tridy a vypiste informace o trávení volného času.
+        // Ulozit do kolekce.
+    }
+}
