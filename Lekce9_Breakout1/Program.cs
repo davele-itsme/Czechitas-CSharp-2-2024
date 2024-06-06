99f86c5ce9e3ad69d5cd66c5eb83e082ca2bc5e6
David Le <ledaodavid@gmail.com>
2024-06-06 17:49:42 +0200

    Breakout1 - lekce9

---
+++ Lekce9_Breakout1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Clovek
+{
+    public string Jmeno {get; set;}
+    public string Prijmeni {get; set;}
+    public int TelCislo {get; set;}
+	
+    public Clovek(string jmeno, string prijmeni, int telCislo)
+    {
+        Jmeno = jmeno;
+        Prijmeni = prijmeni;
+        TelCislo = telCislo;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Clovek> telefonniSeznam = new List<Clovek>
+        {
+            new Clovek("Jarda", "Kadlec", 777123456),
+            new Clovek("Honza", "Kratochvila", 777987654),
+            new Clovek("Petr", "Novak", 778111222)
+        };
+		
+        //1. Vytvorte slozku TelefonniSeznam v adresari Appdata pro konkretniho uzivatele
+        //2. Do souboru telefonniSeznam.csv ulozte obsah seznamu telefonniSeznam tak,
+        //   ze kazdy zaznam bude na jednom radku a oddelene budou carkami
+        //3. Napiste cyklus, ktery soubor precte a zpatky ho ulozi do noveho seznamu
+		
+    }
+}
