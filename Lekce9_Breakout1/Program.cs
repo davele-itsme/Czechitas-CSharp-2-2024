4e2c550663ff60550d88fbf4ca92b09f895a6140
David Le <ledaodavid@gmail.com>
2024-06-06 19:22:46 +0200

    Dokonceny BR1

--- Lekce9_Breakout1/Program.cs
+++ Lekce9_Breakout1/Program.cs
@@ -13,6 +13,11 @@ public class Clovek
         Prijmeni = prijmeni;
         TelCislo = telCislo;
     }
+    
+    public override string ToString()
+    {
+        return $"{Jmeno},{Prijmeni},{TelCislo}";
+    }
 }
 
 public class Program
@@ -27,9 +32,31 @@ public class Program
         };
 		
         //1. Vytvorte slozku TelefonniSeznam v adresari Appdata pro konkretniho uzivatele
+        string cestaKApplicationData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        string cestaKTelefonnimuSeznamu = Path.Combine(cestaKApplicationData, "TelefonniSeznam");
+        if (!Directory.Exists(cestaKTelefonnimuSeznamu))
+        {
+            Directory.CreateDirectory(cestaKTelefonnimuSeznamu);
+        }
+
         //2. Do souboru telefonniSeznam.csv ulozte obsah seznamu telefonniSeznam tak,
         //   ze kazdy zaznam bude na jednom radku a oddelene budou carkami
+        
+        //Predelat si datovy typ cloveka do stringu
+        List<string> obsah = telefonniSeznam.Select(c => c.ToString()).ToList();
+        //Vytvor soubor
+        string cestaKCsvSouboru = Path.Combine(cestaKTelefonnimuSeznamu, "telefonniSeznam.csv");
+        File.WriteAllLines(cestaKCsvSouboru, obsah);
+
         //3. Napiste cyklus, ktery soubor precte a zpatky ho ulozi do noveho seznamu
-		
+        List<Clovek> novySeznam = new List<Clovek>();
+
+        string[] prectenyLines = File.ReadAllLines(cestaKCsvSouboru);
+        foreach (var linka in prectenyLines)
+        {
+            Console.WriteLine(linka);
+        }
+
+        //Preti mi ze souboru telefonniSeznam.csv a vytvor novy seznam ktery bude slozen z objektu Clovek
     }
 }
